71c077fe07e6538630d6f7a12c126622db974799
JeelGajera <83470656+JeelGajera@users.noreply.github.com>
2021-10-07 21:33:07 +0530

    first 2 project

---
+++ P1_Color Flipper/app.js
@@ -0,0 +1,14 @@
+const colors = ["yellow", "red", "rgba(123,345,200)", "#f8b34b"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    //get random number 0-3 
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * colors.length);
+}
